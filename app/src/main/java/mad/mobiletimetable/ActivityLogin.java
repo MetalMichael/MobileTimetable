3028b1ce962b6d30f7ba08639f265dce1c65373c
MetalMichael <michaelfiford@gmail.com>
2014-11-29 20:00:53 +0000

    Remove settings menu from actionbar

--- app/src/main/java/mad/mobiletimetable/ActivityLogin.java
+++ app/src/main/java/mad/mobiletimetable/ActivityLogin.java
@@ -30,27 +30,6 @@ public class ActivityLogin extends Activity{
         fragmentTransaction.commit();
 
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     public void Skip(View view){
         Intent intent = new Intent(this, ActivityMain.class);
         finish();
