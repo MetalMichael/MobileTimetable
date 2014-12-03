5f9e19d7f299c187c4d51d290dc327939fb10940
Clowny <clownyboyxd@gmail.com>
2014-12-03 13:07:29 +0000

    Fix to swipe. Add button, button not function

--- app/src/main/java/mad/mobiletimetable/FragmentTimetable.java
+++ app/src/main/java/mad/mobiletimetable/FragmentTimetable.java
@@ -1,10 +1,14 @@
 package mad.mobiletimetable;
 
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -23,16 +27,33 @@ public class FragmentTimetable extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setHasOptionsMenu(true); //allows menu
         //TODO: Initiate API Requests etc.
     }
 
     @Override
+
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.timetable, menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_add_timetable:
+                //Intent intent = new Intent(getActivity(), ActivityMain.class);
+                //intent.putExtra("FragmentAddToTimetable", "3");
+                //startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View timetable = inflater.inflate(R.layout.fragment_fragment_timetable, container, false);
-        //Check to see if we're in the tablet landscape view
+        //Check to see if we're in the tablet landscape viokkkew
 
         return timetable;
     }
@@ -40,35 +61,37 @@ public class FragmentTimetable extends Fragment {
     public class GlobalInt {
         public int dayOfWeek = 0;
         public int day;
-        public void GlobalInt() {
+        GlobalInt() {
             Calendar c = Calendar.getInstance();
             this.dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-            switch (dayOfWeek) {
+            switch (this.dayOfWeek) {
                 case Calendar.SUNDAY:
-                    day = 0;
+                    this.day = 0;
                     break;
                 case Calendar.MONDAY:
-                    day = 0;
+                    this.day = 0;
                     break;
                 case Calendar.TUESDAY:
-                    day = 1;
+                    this.day = 1;
                     break;
                 case Calendar.WEDNESDAY:
-                    day = 2;
+                    this.day = 2;
                     break;
                 case Calendar.THURSDAY:
-                    day = 3;
+                    this.day = 3;
                     break;
                 case Calendar.FRIDAY:
-                    day = 4;
+                    this.day = 4;
                     break;
                 case Calendar.SATURDAY:
-                    day = 0;
+                    this.day = 0;
                     break;
             }
+            Log.d("Construct",String.valueOf(this.day));
         }
         public int returnDay() {
-               return this.day;
+            Log.d("returnDay",String.valueOf(this.day));
+            return this.day;
         }
         public int increment(){
             this.day ++;
