ebfe6b4a32c30bfc54df2f1c5cb6f8b208bd494a
maael <mae.me.uk@gmail.com>
2014-12-09 01:34:09 +0000

    Correct local IDs & getall updating on add action

--- app/src/main/java/mad/mobiletimetable/APIClass.java
+++ app/src/main/java/mad/mobiletimetable/APIClass.java
@@ -41,35 +41,40 @@ public class APIClass extends APIClassBase {
     }
 
     private int getFileID(HashMap<String,String> requestMap) {
+        Log.d("API Class","*----File ID----*");
         Log.d("API Class","Getting File ID for "+requestMap.toString());
         int id = 1;
-        String dirPath = context.getFilesDir()+"/"+requestMap.get("method")+"/"+requestMap.get("action");
+        String dirPath = context.getFilesDir()+"/"+requestMap.get("method")+"/get";
         File dir = new File(dirPath);
         File dirFiles[] = dir.listFiles();
         if(dirFiles!=null) {
-            Log.d("API Class","We found "+dirFiles.length+" files for "+requestMap.get("method")+" "+requestMap.get("action"));
+            Log.d("API Class","Found "+dirFiles.length+" files for "+requestMap.get("method"));
+            Log.d("API Class","----------");
             for (int i = 0; i < dirFiles.length; i++) {
-                Log.d("API Class","File "+i+"| "+dirFiles[i].getName());
-                int tempid = Integer.parseInt(dirFiles[i].getName().split("-")[0].trim());
-                if (tempid > id) {
+                Log.d("API Class",dirFiles[i].getName());
+                int tempid = Integer.parseInt(dirFiles[i].getName().trim());
+                if (tempid >= id) {
                     id = tempid + 1;
                 }
             }
+            Log.d("API Class","----------");
         }
-        Log.d("API Class","ID for "+requestMap.toString()+" is "+Integer.toString(id));
+        Log.d("API Class","ID is "+Integer.toString(id));
+        Log.d("API Class","*-----------------*");
         return id;
     }
 
     private String getFileName(HashMap<String,String> requestMap){
-        String mapCode = Integer.toString(requestMap.hashCode());
-        String fileName = getFileID(requestMap)+"-"+mapCode;
+        String fileName = Integer.toString(getFileID(requestMap));
+        if(requestMap.get("action").equals("getall")){
+            fileName = "all";
+        }
         return fileName;
     }
 
     // Check local storage for result of request if connection unavailable
     // use it as the result if it is present
     private String fetchFromStorage(HashMap<String,String> requestMap){
-        Log.d("API Class","Fetch from Storage");
         String result = "";
         String dirPath = context.getFilesDir()+"/"+requestMap.get("method")+"/"+requestMap.get("action");
         String fileName = getFileName(requestMap);
@@ -94,32 +99,36 @@ public class APIClass extends APIClassBase {
     }
 
     // Save successful request result to local storage
-    private void saveToStorage(HashMap<String,String> requestMap, String result){
-        Log.d("API Class","Saving to Storage");
+    private String saveToStorage(HashMap<String,String> requestMap, String result){
         String dirPath = context.getFilesDir()+"/"+requestMap.get("method")+"/"+requestMap.get("action");
         String fileName = getFileName(requestMap);
-        Log.d("API Class","Saving "+dirPath+"/"+fileName);
-        FileOutputStream fos = null;
+        Log.d("API Class","Saving to "+dirPath+"/"+fileName);
+        Log.d("API Class","Saving result "+result);
         try {
             File resultFile = new File(dirPath, fileName);
-            if(resultFile.getParentFile().mkdirs()) {
+            resultFile.getParentFile().mkdirs();
+            if(resultFile.getParentFile().exists()) {
                 resultFile.createNewFile();
                 // Save cached result
+                FileOutputStream fos = null;
                 fos = new FileOutputStream(resultFile);
                 fos.write(result.getBytes());
-                fos.flush();
                 fos.close();
             }
         } catch (Exception e){
             // We have a permissions problem or the app directory doesn't exist
             e.printStackTrace();
         }
-        Log.d("API Class","Save Finished");
+        Log.d("API Class","Save Finished "+result);
+        return fileName;
     }
 
     private JSONObject appendToArray(String arrayName, JSONObject newElement, HashMap<String,String> requestMap){
         JSONObject response = new JSONObject();
+        requestMap.put("action","getall");
+        Log.d("API Class","*----Appending----*");
         String previousGetAll = fetchFromStorage(requestMap);
+        Log.d("API Class","Append Previous "+previousGetAll);
         try {
             JSONArray elements = new JSONArray();
             if(previousGetAll != ""){
@@ -132,6 +141,8 @@ public class APIClass extends APIClassBase {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        Log.d("API Class","Append Response "+response.toString());
+        Log.d("API Class","*--Appending End--*");
         return response;
     }
 
@@ -139,10 +150,8 @@ public class APIClass extends APIClassBase {
     private JSONObject formatModuleGet(HashMap<String,String> requestMap,Boolean singleResponse){
         JSONObject response = new JSONObject();
         JSONObject module = new JSONObject();
-        Log.d("API Class",singleResponse.toString());
         try {
-            int id = getFileID(requestMap);
-            module.put("ID",id);
+            module.put("ID",getFileID(requestMap));
             module.put("ModuleCode",requestMap.get("modulecode"));
             module.put("ModuleTitle",requestMap.get("moduletitle"));
             module.put("Lecturer",requestMap.get("lecturer"));
@@ -158,10 +167,8 @@ public class APIClass extends APIClassBase {
         return response;
     }
     private JSONObject formatModuleGetAll(HashMap<String,String> requestMap){
-        JSONObject response = new JSONObject();
         JSONObject newModule = formatModuleGet(requestMap,false);
-        response = appendToArray("modules",newModule,requestMap);
-        return response;
+        return appendToArray("modules",newModule,requestMap);
     }
 
     // Format a timetable add string to a timetable get string response
@@ -192,10 +199,8 @@ public class APIClass extends APIClassBase {
     }
 
     private JSONObject formatTimetableGetAll(HashMap<String,String> requestMap){
-        JSONObject response = new JSONObject();
         JSONObject newEvent = formatTimetableGet(requestMap,false);
-        response = appendToArray("events",newEvent,requestMap);
-        return response;
+        return appendToArray("events",newEvent,requestMap);
     }
 
     private String performGenericAdd(HashMap<String,String> requestMap){
@@ -208,22 +213,26 @@ public class APIClass extends APIClassBase {
         HashMap<String,String> getRequest = new HashMap<String, String>();
         getRequest.put("method",method);
         getRequest.put("action", "getall");
+        String formattedResponse = null;
+        Log.d("API Class",">Getall updating<");
         if(method.equals("module")) {
-            saveToStorage(getRequest, formatModuleGetAll(requestMap).toString());
+            formattedResponse = formatModuleGetAll(requestMap).toString();
         } else {
-            saveToStorage(getRequest,formatTimetableGetAll(requestMap).toString());
+            formattedResponse = formatTimetableGetAll(requestMap).toString();
         }
-        getRequest.put("action","get");
-        String fileID = Integer.toString(getFileID(requestMap));
-        getRequest.put(id, fileID);
+        saveToStorage(getRequest,formattedResponse);
+        Log.d("API Class",">Get creation<");
+        getRequest.put("action", "get");
         if(method.equals("module")) {
-            saveToStorage(getRequest,formatModuleGet(requestMap,true).toString());
+            formattedResponse = formatModuleGet(requestMap,true).toString();
         } else {
-            saveToStorage(getRequest,formatTimetableGet(requestMap,true).toString());
+            formattedResponse = formatTimetableGet(requestMap,true).toString();
         }
+        String fileID = saveToStorage(getRequest,formattedResponse);
         JSONObject response = new JSONObject();
         try {
             response.put(id, fileID);
+            response.put("status","OK");
         } catch (JSONException e){
             e.printStackTrace();
         }
@@ -231,29 +240,32 @@ public class APIClass extends APIClassBase {
     }
 
     private void performGenericDelete(HashMap<String,String> requestMap){
+        Log.d("API Class","Deleting a "+requestMap.get("method"));
         String fileName = getFileName(requestMap);
         context.deleteFile(fileName);
     }
 
     private void performGenericEdit(HashMap<String,String> requestMap){
-        JSONObject original = new JSONObject();
+        Log.d("API Class","Editing a "+requestMap.get("method"));
+        JSONObject newResponse = new JSONObject();
         requestMap.put("action","get");
         if(requestMap.get("method").equals("module")){
-            original = formatModuleGet(requestMap,true);
+            newResponse = formatModuleGet(requestMap,true);
         } else if(requestMap.get("method").equals("timetable")) {
-            original = formatTimetableGet(requestMap,true);
+            newResponse = formatTimetableGet(requestMap,true);
         }
+        Log.d("API Class","Original | "+newResponse.toString());
     }
 
     @Override
     protected String localHandler(HashMap<String,String> requestMap){
         String result = "";
-        Log.d("API Class","localHandler Called");
         String method = requestMap.get("method");
         String action = requestMap.get("action");
+        Log.d("API Class","localHandler Called for "+method+" "+action);
         if(method.equals("module")||method.equals("timetable")){
             if(action.equals("add")){
-                performGenericAdd(requestMap);
+                result = performGenericAdd(requestMap);
             } else if(action.equals("get")){
                 result = fetchFromStorage(requestMap).toString();
             } else if(action.equals("getall")){
